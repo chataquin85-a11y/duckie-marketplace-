baadd4cb34a67ce1ff672862f4445116195d718e
chataquin85-a11y <chataquin85@gmail.com>
2026-05-30 21:51:44 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-// Prueba de vida del script
-console.log("¡El script.js se ha cargado correctamente!");
 
-// Verificar si Firebase está inicializado
-try {
-  const auth = firebase.auth();
-  console.log("Firebase Auth está listo.");
-} catch (error) {
-  console.error("Error al conectar con Firebase:", error);
-}
